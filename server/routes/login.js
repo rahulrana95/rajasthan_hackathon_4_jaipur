b5886a9fb50adbe667a3c054169cf41eb5d0fcd7
Rahul Rana <3rahul4@gmail.com>
2018-03-19 18:26:07 +0530

    login api working

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,9 +1,24 @@
 import express from 'express';
 const router = express.Router();
-const login = router.get('/login',function(request,response){
-	response.send({
-		status:200
+const login = router.post('/login',function(request,response){
+	let data = request.body.data;
+	const db = request.app.locals.settings.db;
+	const user = db.collection('user');
+	let username = data.username;
+	let password = data.password;
+	user.find({username:username,password:password}).toArray((error,result)=>{
+		if(error){
+			response.json({
+				status:404,
+				error:error
+			});
+		} else {
+			response.json({
+				status:404,
+				result:result
+			});
+		}
 	});
-});
+	});
 
 module.exports = login;
